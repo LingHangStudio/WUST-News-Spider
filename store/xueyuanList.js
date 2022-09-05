8d03c0980dc90b9c5d8df578cb72705b38bbf634
Monkeyhbd <monkeyhbd@163.com>
2022-09-05 12:27:21 +0800

    新增学院列表

---
+++ store/xueyuanList.js
@@ -0,0 +1,496 @@
+var list = {
+	cailiaoyuyejinxueyuan: [  // 材料与冶金学院
+		{
+			house: 1,
+			sub: 1,
+			name: "gonggaolan",
+			cnName: "公告栏",
+			url: "https://cy.wust.edu.cn/878/list.htm"
+		},
+		{
+			house: 1,
+			sub: 2,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://cy.wust.edu.cn/776/list.htm"
+		},
+		{
+			house: 1,
+			sub: 3,
+			name: "xuegongdongtai",
+			cnName: "学工动态",
+			url: "https://cy.wust.edu.cn/581/list.htm"
+		},
+		{
+			house: 1,
+			sub: 4,
+			name: "xueshujiaoliu",
+			cnName: "学术交流",
+			url: "https://cy.wust.edu.cn/547/list.htm"
+		},
+		{
+			house: 1,
+			sub: 5,
+			name: "shiyanshianquan",
+			cnName: "实验室安全",
+			url: "https://cy.wust.edu.cn/778/list.htm"
+		}
+	],
+
+	chengshijianshexueyuan: [  // 城市建设学院
+		{
+			house: 2,
+			sub: 1,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/cjxy/932/list.htm"
+		},
+		{
+			house: 2,
+			sub: 2,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/cjxy/933/list.htm"
+		},
+		{
+			house: 2,
+			sub: 3,
+			name: "rencaipeiyang",
+			cnName: "人才培养",
+			url: "https://www.wust.edu.cn/cjxy/978/list.htm"
+		}
+	],
+
+	hengdaguanlixueyuan: [  // 恒大管理学院
+		{
+			house: 3,
+			sub: 1,
+			name: "xueyuanyaowen",
+			cnName: "学院要闻",
+			url: "https://som.wust.edu.cn/3477/list.htm"
+		},
+		{
+			house: 3,
+			sub: 2,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://som.wust.edu.cn/3478/list.htm"
+		},
+		{
+			house: 3,
+			sub: 3,
+			name: "xuegongdongtai",
+			cnName: "学术动态",
+			url: "https://som.wust.edu.cn/3479/list.htm"
+		},
+		{
+			house: 3,
+			sub: 4,
+			name: "xuezijiayuan",
+			cnName: "学子家园",
+			url: "https://som.wust.edu.cn/3480/list.htm"
+		}
+	],
+
+	guojixueyuan: [  // 国际学院
+		{
+			house: 4,
+			sub: 1,
+			name: "xinwendongtai",
+			cnName: "新闻动态",
+			url: "https://wis.wust.edu.cn/xwdt/list.htm"
+		},
+		{
+			house: 4,
+			sub: 2,
+			name: "gonggaolan",
+			cnName: "公告栏",
+			url: "https://wis.wust.edu.cn/ggl/list.htm"
+		},
+		{
+			house: 4,
+			sub: 3,
+			name: "jiaoxuexinxi",
+			cnName: "教学信息",
+			url: "https://wis.wust.edu.cn/zsdt/list.htm"
+		}
+	],
+
+	huaxueyuhuagongxueyuan: [  // 化学与化工学院
+		{
+			house: 5,
+			sub: 1,
+			name: "xueyuanyaowen",
+			cnName: "学院要闻",
+			url: "https://www.wust.edu.cn/hxyhg/2184/list.htm"
+		},
+		{
+			house: 5,
+			sub: 2,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/hxyhg/2183/list.htm"
+		},
+		{
+			house: 5,
+			sub: 3,
+			name: "jiaoxuedongtai",
+			cnName: "教学动态",
+			url: "https://www.wust.edu.cn/hxyhg/2154/list.htm"
+		}
+	],
+
+	jixiezidonghuaxueyuan: [  // 机械自动化学院
+		{
+			house: 6,
+			sub: 1,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/jxzdh/1424/list.htm"
+		},
+		{
+			house: 6,
+			sub: 2,
+			name: "xueshujiaoliu",
+			cnName: "学术交流",
+			url: "https://www.wust.edu.cn/jxzdh/1427/list.htm"
+		},
+		{
+			house: 6,
+			sub: 3,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/jxzdh/1423/list.htm"
+		},
+		{
+			house: 6,
+			sub: 4,
+			name: "jiaoyujiaoxue",
+			cnName: "教育教学",
+			url: "https://www.wust.edu.cn/jxzdh/1425/list.htm"
+		},
+		{
+			house: 6,
+			sub: 5,
+			name: "shishengdongtai",
+			cnName: "师生动态",
+			url: "https://www.wust.edu.cn/jxzdh/1426/list.htm"
+		}
+	],
+
+	jisuanjikexueyujishuxueyuan: [  // 计算机科学与技术学院
+		{
+			house: 7,
+			sub: 1,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/jsjkx/1563/list.htm"
+		},
+		{
+			house: 7,
+			sub: 2,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/jsjkx/1564/list.htm"
+		},
+		{
+			house: 7,
+			sub: 3,
+			name: "dangjiangongzuo",
+			cnName: "党建工作",
+			url: "https://www.wust.edu.cn/jsjkx/1556/list.htm"
+		}
+	],
+
+	lixueyuan: [  // 理学院
+		{
+			house: 8,
+			sub: 1,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://lixueyuan.wust.edu.cn/2196/list.htm"
+		},
+		{
+			house: 8,
+			sub: 2,
+			name: "jiaoxuedongtai",
+			cnName: "教学动态",
+			url: "https://lixueyuan.wust.edu.cn/2197/list.htm"
+		},
+		{
+			house: 8,
+			sub: 3,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://lixueyuan.wust.edu.cn/2195/list.htm"
+		},
+		{
+			house: 8,
+			sub: 4,
+			name: "keyandongtai",
+			cnName: "科研动态",
+			url: "https://lixueyuan.wust.edu.cn/2220/list.htm"
+		}
+	],
+
+	makesizhuyixueyuan: [  // 马克思主义学院
+		{
+			house: 9,
+			sub: 1,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://my.wust.edu.cn/2118/list.htm"
+		},
+		{
+			house: 9,
+			sub: 2,
+			name: "xiaoyuanduanbo",
+			cnName: "校园短波",
+			url: "https://my.wust.edu.cn/2117/list.htm"
+		},
+		{
+			house: 9,
+			sub: 3,
+			name: "lilunguangjiao",
+			cnName: "理论广角",
+			url: "https://my.wust.edu.cn/2119/list.htm"
+		},
+		{
+			house: 9,
+			sub: 4,
+			name: "shehuijujiao",
+			cnName: "社会聚焦",
+			url: "https://my.wust.edu.cn/2120/list.htm"
+		}
+	],
+
+	qicheyujiaotonggongchengxueyuan: [  // 汽车与交通工程学院
+		{
+			house: 10,
+			sub: 1,
+			name: "xinwentoutiao",
+			cnName: "新闻头条",
+			url: "https://www.wust.edu.cn/qcyjt/2078/list.htm"
+		},
+		{
+			house: 10,
+			sub: 2,
+			name: "zuixingonggao",
+			cnName: "最新公告",
+			url: "https://www.wust.edu.cn/qcyjt/2078/list.htm"
+		}
+	],
+
+	shengmingkexueyujiankangxueyuan: [  // 生命科学与健康学院
+		{
+			house: 11,
+			sub: 1,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/smkx/832/list.htm"
+		},
+		{
+			house: 11,
+			sub: 2,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/smkx/831/list.htm"
+		},
+		{
+			house: 11,
+			sub: 3,
+			name: "guanlizhidu",
+			cnName: "管理制度",
+			url: "https://www.wust.edu.cn/smkx/3546/list.htm"
+		},
+		{
+			house: 11,
+			sub: 4,
+			name: "xueshujiaoliu",
+			cnName: "学术交流",
+			url: "https://www.wust.edu.cn/smkx/858/list.htm"
+		},
+		{
+			house: 11,
+			sub: 5,
+			name: "keyandongtai",
+			cnName: "科研动态",
+			url: "https://www.wust.edu.cn/smkx/835/list.htm"
+		}
+	],
+
+	tiyuxueyuan: [  // 体育学院
+		{
+			house: 12,
+			sub: 1,
+			name: "xinwendongtai",
+			cnName: "新闻动态",
+			url: "https://www.wust.edu.cn/tyxy/765/list.htm"
+		},
+		{
+			house: 12,
+			sub: 2,
+			name: "dangjiangongzuo",
+			cnName: "党建工作",
+			url: "https://www.wust.edu.cn/tyxy/765/list.htm"
+		},
+		{
+			house: 12,
+			sub: 3,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/tyxy/765/list.htm"
+		}
+	],
+
+	waiguoyuxueyuan: [  // 外国语学院
+		{
+			house: 13,
+			sub: 1,
+			name: "gongshigonggao",
+			cnName: "公示公告",
+			url: "https://www.wust.edu.cn/wgy/3285/list.htm"
+		},
+		{
+			house: 13,
+			sub: 2,
+			name: "zonghexinwen",
+			cnName: "综合新闻",
+			url: "https://www.wust.edu.cn/wgy/3286/list.htm"
+		},
+		{
+			house: 13,
+			sub: 3,
+			name: "dangjiangongzuo",
+			cnName: "党建工作",
+			url: "https://www.wust.edu.cn/wgy/3280/list.htm"
+		},
+		{
+			house: 13,
+			sub: 4,
+			name: "xueshenggongzuo",
+			cnName: "学生工作",
+			url: "https://www.wust.edu.cn/wgy/3281/list.htm"
+		}
+	],
+
+	wenfayujingjixueyuan: [  // 文法与经济学院
+		{
+			house: 14,
+			sub: 1,
+			name: "gonggaolan",
+			cnName: "公告栏",
+			url: "https://www.wust.edu.cn/wfyjj/1658/list.htm"
+		},
+		{
+			house: 14,
+			sub: 2,
+			name: "xinwendongtai",
+			cnName: "新闻动态",
+			url: "https://www.wust.edu.cn/wfyjj/1657/list.htm"
+		},
+		{
+			house: 14,
+			sub: 3,
+			name: "shishengdongtai",
+			cnName: "师生风采",
+			url: "https://www.wust.edu.cn/wfyjj/1660/list.htm"
+		}
+	],
+
+	xinxikexueyugongchengxueyuan: [  // 信息科学与工程学院
+		{
+			house: 15,
+			sub: 1,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/xxkx/6043/list.htm"
+		},
+		{
+			house: 15,
+			sub: 2,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/xxkx/xyxwcs/list.htm"
+		}
+	],
+
+	yixueyuan: [  // 医学院
+		{
+			house: 16,
+			sub: 1,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/yxy/1337/list.htm"
+		},
+		{
+			house: 16,
+			sub: 2,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/yxy/1030/list.htm"
+		},
+		{
+			house: 16,
+			sub: 3,
+			name: "keyantongzhi",
+			cnName: "科研通知",
+			url: "https://www.wust.edu.cn/yxy/1033/list.htm"
+		},
+		{
+			house: 16,
+			sub: 4,
+			name: "xuetuandongtai",
+			cnName: "学团动态",
+			url: "https://www.wust.edu.cn/yxy/1035/list.htm"
+		}
+	],
+
+	yishuyushejixueyuan: [  // 艺术与设计学院
+		{
+			house: 17,
+			sub: 1,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://www.wust.edu.cn/ysysj/1370/list.htm"
+		},
+		{
+			house: 17,
+			sub: 2,
+			name: "xueyuanxinwen",
+			cnName: "学院新闻",
+			url: "https://www.wust.edu.cn/ysysj/1371/list.htm"
+		}
+	],
+
+	ziyuanyuhuanjinggongchengxueyuan: [  // 资源与环境工程学院
+		{
+			house: 1,
+			sub: 1,
+			name: "tongzhigonggao",
+			cnName: "通知公告",
+			url: "https://cree.wust.edu.cn/1754/list.htm"
+		},
+		{
+			house: 1,
+			sub: 2,
+			name: "jiaoxuedongtai",
+			cnName: "教学动态",
+			url: "https://cree.wust.edu.cn/1726/list.htm"
+		},
+		{
+			house: 1,
+			sub: 3,
+			name: "xuegongdongtai",
+			cnName: "学工动态",
+			url: "https://cree.wust.edu.cn/1749/list.htm"
+		},
+		{
+			house: 1,
+			sub: 4,
+			name: "keyandongtaii",
+			cnName: "科研动态",
+			url: "https://cree.wust.edu.cn/1736/list.htm"
+		}
+	],
+}
